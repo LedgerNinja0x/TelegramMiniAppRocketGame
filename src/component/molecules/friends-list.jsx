401a13d7f799172838fb5403106b12dada43c370
LedgerNinja0x <sweetheart199558@gmail.com>
2024-08-14 14:12:43 +1000

    fix: friend-image

--- src/component/molecules/friends-list.jsx
+++ src/component/molecules/friends-list.jsx
@@ -3,13 +3,12 @@ import FriendInfo from "../atom/friend-info";
 
 const FriendsList = ( { friendData } ) => {
     return (
-        <div className="flex-auto" style={{height: "calc(100vh - 500px)"}}>
+        <div className="flex-auto flex" style={{height: "calc(100vh - 500px)"}}>
             {
                 friendData.length > 0 ? 
                 (
                     <>
-                        <AtomLabel content={"List of your friends"} />
-                        <div className="flex flex-col gap-2 mt-4">
+                        <div className="flex flex-col gap-2 w-full">
                             {
                                 friendData.map((_friend, _index) => {
                                     return (
@@ -20,11 +19,11 @@ const FriendsList = ( { friendData } ) => {
                         </div>
                     </>
                 ) : (
-                    <div className="py-[30px] flex flex-col items-center gap-4 text-center my-auto h-full">
+                    <div className="flex flex-col items-center gap-4 text-center my-auto">
                         <img 
                             src="/image/main/friends.png"
                             alt=""
-                            className="h-full max-w-auto"
+                            className="max-w-auto"
                         />
                         <div className="text-[15px] text-white">
                             Invite a friend and you'll both get 100 points. Every time the invited person tops up their TON balance, you will receive 1% of the top-up amount.
