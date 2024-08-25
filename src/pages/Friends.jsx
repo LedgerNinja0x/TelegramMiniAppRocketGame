3d83ec0b296ffd80e2e769895c5cd7ae19105b50
trinklestar58 <juliosantos950527@gmail.com>
2024-08-26 03:55:04 +0900

    Fix : Friend.js

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -80,8 +80,8 @@ const shareInviteLink = (inviteLink) => {
 
     return (
         <div className="flex flex-col h-full gap-4 pb-[76px] justify-between">
-            <FriendComment friendData={friendList} />
-            <FriendsList friendData={friendList} />
+            {/* <FriendComment friendData={friendList} />
+            <FriendsList friendData={friendList} /> */}
             <FriendEarned setIsModalOpen={setIsOpen} />
             <ScrollModal icon={<NavFriends />} title={"Invite a Friend"} isOpen={isOpen} setIsOpen={setIsOpen}>
                 <div className="pb-6 flex flex-col gap-4 px-4">
