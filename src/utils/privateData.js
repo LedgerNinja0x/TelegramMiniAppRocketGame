e79b075de3393a83ad9242884f79ed285cd858a0
trinklestar58 <juliosantos950527@gmail.com>
2024-08-27 05:17:52 +0900

    Fix server Url on vps

--- src/utils/privateData.js
+++ src/utils/privateData.js
@@ -1,4 +1,4 @@
-  export const REACT_APP_SERVER = "https://telegramminiapp-rocket-backend.onrender.com";
+  export const REACT_APP_SERVER = "https://0be7-167-88-165-3.ngrok-free.app/";
   export const REACT_APP_SERVER1 = "http://localhost:5000";
   export const REACT_APP_WS_SERVER = "wss://telegramminiapp-rocket-backend.onrender.com";
   export const REACT_APP_WS_SERVER1 = "ws://localhost:5000";
