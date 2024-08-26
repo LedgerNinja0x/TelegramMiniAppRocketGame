36281326dffce09447f2a6f7f1306161d5c28b40
trinklestar58 <juliosantos950527@gmail.com>
2024-08-26 14:44:35 +0900

    Revert "test: clipboard-3"
    This reverts commit ea70c73fdcb0c447f83c16ed7d7766206a3e7e0e.

--- src/component/atom/shadow-btn.jsx
+++ src/component/atom/shadow-btn.jsx
@@ -4,7 +4,7 @@ const ShadowButton = ( {children, className, action, disabled} ) => {
     return (
         <button 
             className={cn("rounded-[12px] w-full text-center py-[14px] text-[16px] font-bold cursor-pointer border border-solid border-transparent shadow-btn-blue-border bg-[#3434DA] text-white invite-btn-shadow", className)}
-            onClick={action && action}
+            onClick={action}
             disabled={disabled}
         >
             { children }
