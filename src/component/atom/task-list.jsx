8f599c6fc42943d916e10cd8211860a4d335a75b
trinklestar58 <juliosantos950527@gmail.com>
2024-08-27 04:34:16 +0900

    test: task-8

--- src/component/atom/task-list.jsx
+++ src/component/atom/task-list.jsx
@@ -11,52 +11,74 @@ const serverUrl = REACT_APP_SERVER;
 
 
 
-// const GenerateTask = (_task, _index, stateTask) => {
+const GenerateTask = (_task, _index, stateTask) => {
 
-//   const [isClaim, setIsClaim] = useState(false);
-//   const [user, setUser] = useAtom(userData);
+  // const [isClaim, setIsClaim] = useState(false);
+  // const [user, setUser] = useAtom(userData);
 
-//   const goClaim = () => {
-//     // setIsClaim(true);
-//     // const headers = new Headers()
-//     // headers.append('Content-Type', 'application/json')
-//     // fetch(`${serverUrl}/task_balance`, { method: 'POST', body: JSON.stringify({ userName: user.UserName, amount: _task.amount, task: _index }), headers })
-//     //   .then(res => Promise.all([res.status, res.json()]))
-//     //   .then(() => {
-//     //     try {
-//     //       toast(`${_task.amount} coins added to your balance`,
-//     //         {
-//     //           position: "top-center",
-//     //           icon: <CheckMark />,
-//     //           style: {
-//     //             borderRadius: '8px',
-//     //             background: '#7886A0',
-//     //             color: '#fff',
-//     //             width: '90vw'
-//     //           },
-//     //         }
-//     //       )
-//     //     } catch (e) {
-//     //       // eslint-disable-next-line no-self-assign
-//     //       document.location.href = document.location.href
-//     //     }
-//     //     setIsClaim(false)
-//     //     stateTask()
-//     //   })
-//   }
+  // const goClaim = () => {
+  //   // setIsClaim(true);
+  //   // const headers = new Headers()
+  //   // headers.append('Content-Type', 'application/json')
+  //   // fetch(`${serverUrl}/task_balance`, { method: 'POST', body: JSON.stringify({ userName: user.UserName, amount: _task.amount, task: _index }), headers })
+  //   //   .then(res => Promise.all([res.status, res.json()]))
+  //   //   .then(() => {
+  //   //     try {
+  //   //       toast(`${_task.amount} coins added to your balance`,
+  //   //         {
+  //   //           position: "top-center",
+  //   //           icon: <CheckMark />,
+  //   //           style: {
+  //   //             borderRadius: '8px',
+  //   //             background: '#7886A0',
+  //   //             color: '#fff',
+  //   //             width: '90vw'
+  //   //           },
+  //   //         }
+  //   //       )
+  //   //     } catch (e) {
+  //   //       // eslint-disable-next-line no-self-assign
+  //   //       document.location.href = document.location.href
+  //   //     }
+  //   //     setIsClaim(false)
+  //   //     stateTask()
+  //   //   })
+  // }
 
-//   return (
-//     <></>
-//   )
-// }
-
-const generateTask = (task, index, stateTask) => {
-  console.log(task, index, stateTask);
   return (
-    <>
-    </>
+    <div className="bg-[#0000001A] rounded-lg flex justify-between items-center py-2 pl-2 pr-4 text-[14px]" key={_index}>
+      <div className="flex gap-2 items-center">
+        <img src={`/image/icon/${_task.src}`} alt="" className="w-8 h-8" />
+        <div className="flex flex-col">
+          <div className="text-white">{_task.title}</div>
+          <div className="text-[#ffffff99]">+{_task.amount} Coins</div>
+        </div>
+      </div>
+      {
+        _task.status == 0 ?
+          <Link to={'/play'}>
+            <button className="rounded-lg w-[61px] py-1 px-0 h-7 bg-[#3861FB] text-white text-center text-[14px]" >
+              Start
+            </button>
+          </Link> :
+          _task.status == 1 ?
+            <button
+              className="rounded-lg w-[61px] py-1 px-0 h-7 bg-white text-[#080888] text-center text-[14px]"
+              onClick={goClaim}
+            >
+              {
+                isClaim ?
+                  <LoadingSpinner className="w-4 h-4 mx-auto" /> :
+                  "Claim"
+              }
+            </button> :
+            <div className="text-white">
+              <CheckMark />
+            </div>
+      }
+    </div>
   )
-};
+}
 
 const TaskList = () => {
   let taskState = [];
@@ -126,7 +148,7 @@ const TaskList = () => {
   return (
     <div className="flex flex-col gap-2 text-[14px] overflow-auto pb-4" style={{ height: "calc(100vh - 200px)" }}>
       {
-        taskData.map((_task, _index) => generateTask(_task, _index, stateTask))
+        taskData.map((_task, _index) => GenerateTask(_task, _index, stateTask))
       }
     </div>
   )
